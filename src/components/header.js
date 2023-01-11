a5fb7824d28c24870b31d8a3c78e3a8178c8a718
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-01-11 12:31:22 +0800

    202230111 class

--- src/components/header.js
+++ src/components/header.js
@@ -2,29 +2,37 @@ import { useState } from "react";
 
 export default function Header({ menu1 }) {
 
-    const [navBGColor, setNavBGColor] = useState (false)
+    const [navBGColor, setNavBGColor] = useState(false)
 
-    function changeNavBackgroundColor () {
-      if (window.scrollY >= 540){
-        setNavBGColor(true)
-      }else{
-        setNavBGColor(false)
-      }
+    function changeNavBackgroundColor() {
+        if (window.scrollY >= 540) {
+            setNavBGColor(true)
+        } else {
+            setNavBGColor(false)
+        }
+    }
+
+    function SignIn() {
+
     }
 
     window.addEventListener("scroll", changeNavBackgroundColor);
-  
+
     return (
-        <nav  id={navBGColor? "navWhite" : "navOrange" }>
+        <nav id={navBGColor ? "navWhite" : "navOrange"}>
             <div id="headerContainer">
                 <img src={require("../pic/logo1.png")} alt="" width="169px" />
                 <div id="menus">
                     {menu1.map((e) => {
-                        return (
-                            <span className="menu">{e}</span>
-                        )
+                        if (e == "Sign in") {
+                            return (<span className="menu" onClick={SignIn} id="1">{e}</span>)
+                        } else {
+                            return (
+                                <span className="menu">{e}</span>
+                            )
+                        }
                     })}
-                    <span id="getStarted">Get started</span>
+                    <span id={navBGColor ? "getStartedGreen" : "getStartedOrange"}>Get started</span>
                 </div>
             </div>
         </nav>
